439e7744b11d27ee2271c8c43dcc532deaaa3aed
Kingcedru <byishimocedrick@gmail.com>
2024-03-13 13:06:39 +0200

    created message

--- client/websocket/src/App.jsx
+++ client/websocket/src/App.jsx
@@ -1,18 +1,16 @@
-import { useState } from 'react'
-import io from "socket.io-client"
-const socket = io.connet("http://localhost:3000");
-
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:3001");
 
 function App() {
-const sendMessage = ()=>{
-  socket.emit()
-}
+  const sendMessage = () => {
+    socket.emit("send_message", { message: "Hello" });
+  };
   return (
-    <div className='App'>
-     <input type='text' placeholder='Message...'/>
-     <button onClick={sendMessage}>Send Message</button>
+    <div className="App">
+      <input type="text" placeholder="Message..." />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -10,9 +10,19 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://localhost:5173",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST"],
   },
 });
 
-server.listen(3000, ()=>{console.log("Server is running")})
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("send_message", (data) => {
+    console.log(data);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("Server is running");
+});
