03383d2660bdb1dab05cfb390000b25f57445add
Kingcedru <byishimocedrick@gmail.com>
2024-03-13 12:21:21 +0200

    create connection

--- client/websocket/src/App.jsx
+++ client/websocket/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-import './App.css'
+import io from "socket.io-client"
+const socket = io.connet("http://localhost:3000");
+
 
 function App() {
-
+const sendMessage = ()=>{
+  socket.emit()
+}
   return (
-    <>
+    <div className='App'>
      <input type='text' placeholder='Message...'/>
-     <button>Send Message</button>
-    </>
+     <button onClick={sendMessage}>Send Message</button>
+    </div>
   )
 }
 
