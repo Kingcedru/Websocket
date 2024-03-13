0f0a6e336843ecfe9e51a8021556f03aefa9385a
Kingcedru <byishimocedrick@gmail.com>
2024-03-13 15:06:30 +0200

    getting data from a form

--- client/websocket/src/App.jsx
+++ client/websocket/src/App.jsx
@@ -1,13 +1,26 @@
+import { useEffect, useState } from "react";
 import io from "socket.io-client";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
+  const [message, setMessage] = useState("");
   const sendMessage = () => {
     socket.emit("send_message", { message: "Hello" });
   };
+
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      alert(data.message);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <input type="text" placeholder="Message..." />
+      <input
+        type="text"
+        placeholder="Message..."
+        onChange={(event) => setMessage(event.target.value)}
+      />
       <button onClick={sendMessage}>Send Message</button>
     </div>
   );
