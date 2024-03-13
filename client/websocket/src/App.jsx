9f3e0566b3dc4a3cf4c290596d0ba5c1872d83d9
Kingcedru <byishimocedrick@gmail.com>
2024-03-13 15:15:55 +0200

    sending a message

--- client/websocket/src/App.jsx
+++ client/websocket/src/App.jsx
@@ -4,15 +4,16 @@ const socket = io.connect("http://localhost:3001");
 
 function App() {
   const [message, setMessage] = useState("");
+  const [messageReceived, setMessaeReceived] = useState("");
   const sendMessage = () => {
-    socket.emit("send_message", { message: "Hello" });
+    socket.emit("send_message", { message });
   };
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      alert(data.message);
+      setMessaeReceived(data.message);
     });
-  }, []);
+  }, [socket]);
 
   return (
     <div className="App">
@@ -22,6 +23,8 @@ function App() {
         onChange={(event) => setMessage(event.target.value)}
       />
       <button onClick={sendMessage}>Send Message</button>
+      <h1>Messages Received : </h1>
+      <p>{messageReceived}</p>
     </div>
   );
 }
